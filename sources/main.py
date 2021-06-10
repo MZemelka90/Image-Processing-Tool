d5ef3b2549b104fde6a3cc430f67547c195d8c47
erstmalgucken <pham_long1995@yahoo.com>
2021-06-10 12:51:51 +0200

    Maincode

--- sources/main.py
+++ sources/main.py
@@ -1,1 +1,31 @@
 import numpy as np
+from PIL import Image, Imagedraw
+from skimage import data, img_as_float
+# load image
+input_image = Image.open(r'bild.png')
+input_pixels = input_image_load()
+width, height = input_image.width, input_image.height
+
+# Create output image
+output_image = Image.new("RGB", input_image.size)
+draw = Imagedraw.Draw(output_image)
+
+# convert to grayscale
+intensity = np.zeros((width, height))
+for x in range(width):
+    for y in range(height):
+        intensity[x,y] = sum(input_pixels[x, y]) / 3
+
+# Compute convolution between intensity and kernels
+for x in range(1, input_image.width -1):
+    for y in range(1, input_image.height - 1):
+        magx = intensity[x+1,y] - intensity [x-1, y]
+        magy = intensity[x, y + 1] - intensity[x, y - 1]
+
+        # Draw in black and white the magnitude
+        color = int(sqrt(magx**2 + magy**2))
+        draw.point((x, y), (color, color, color))
+output_image.save("output.png")
+
+
+
